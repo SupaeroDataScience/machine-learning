ec48adfa7812fa6287d9e6582101272cfc317caa
Emmanuel Rachelson <emmanuel.rachelson@isae-supaero.fr>
2021-10-12 14:39:50 +0200

    corrected a mistake in the solution of ex1 in SVM notebook

--- 3 - SVMs, bias-variance, and kernels/solutions/code1.py
+++ 3 - SVMs, bias-variance, and kernels/solutions/code1.py
@@ -2,7 +2,8 @@
 # If you get stuck, uncomment the line above to load a correction in this cell (then you can execute this code).
 
 w = np.array([0.7, -1])
-w = w/np.linalg.norm(w)
-w0 = 0.4/np.linalg.norm(w)
+w_norm = np.linalg.norm(w)
+w = w/w_norm
+w0 = 0.4/w_norm
 M = y*(np.dot(X,w) + w0)
 print("margin =", np.min(M))
