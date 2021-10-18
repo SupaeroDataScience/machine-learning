0c41cc0c05da427a4b3602ff0cb439300e8a5186
Emmanuel Rachelson <emmanuel.rachelson@isae-supaero.fr>
2021-10-18 19:21:33 +0200

    ongoing work on the SVM notebook

--- 3 - SVMs, bias-variance, and kernels/solutions/code3.py
+++ 3 - SVMs, bias-variance, and kernels/solutions/code3.py
@@ -13,22 +13,12 @@ print("SV per class:", mySVC.n_support_)
 print("w_i:", w)
 print("w_0:", w0)
 print("Margin:", M)
-print("w^T x0 + w_0:", np.dot(w,mySVC.support_vectors_[0,:])+w0)
-print("w^T x1 + w_0:", np.dot(w,mySVC.support_vectors_[1,:])+w0)
-print("w^T x2 + w_0:", np.dot(w,mySVC.support_vectors_[2,:])+w0)
+print("Training score:", mySVC.score(X,y))
 
 # Plot the separating plane, the margin and the Support Vectors
 fig=plt.figure(figsize=fig_size, dpi= 80, facecolor='w', edgecolor='k')
 plt.scatter(Xblue[:,0],Xblue[:,1],c='b',s=20)
 plt.scatter(Xred[:,0],Xred[:,1],c='r',s=20)
 XX = np.arange(np.min(X[:,0]),np.max(X[:,0]),0.1)
-#XX = np.arange(-1.,12.,0.1)
 YY = -(w[0]*XX+w0)/w[1]
-plt.plot(XX,YY,'g')
-#YY = -(w[0]*XX+w0+M)/w[1]
-#plt.plot(XX,YY,'g--')
-#YY = -(w[0]*XX+w0-M)/w[1]
-#plt.plot(XX,YY,'g--');
-#plt.ylim(-6,10);
-#plt.scatter(mySVC.support_vectors_[:,0], mySVC.support_vectors_[:,1], s=80, edgecolors='k', facecolors='none');
-print("Training score:", mySVC.score(X,y))
+plt.plot(XX,YY,'g');
